71f32fb270664bea663fea402a7a3dff26e71ece
MM-Mamunne <gamesmamun8@gmail.com>
2024-09-18 12:52:56 +0600

    version 2 added

--- Show_Me_API/routes/frnd.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { friends, frndNew } from '../friendControllers/friend.controllers.js';
-// import {signupNew,login, userCheck} from '../signupControllers/signup.controllers.js'
-
-router.post('/new',frndNew);
-router.get('/frnds/:user',friends);
-export default router;
